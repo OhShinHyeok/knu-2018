27e0cbb8116f4358cc535e74b17c1d0dfb36a23d
OhShinhyeok <osh970927@gmail.com>
2018-12-14 18:37:05 +0900

    Fix issue #15 and modify naver api

--- src/main/java/kr/ac/knu/lecture/game/blackjack/Hand.java
+++ src/main/java/kr/ac/knu/lecture/game/blackjack/Hand.java
@@ -24,13 +24,24 @@ public class Hand {
     }
 
     public int getCardSum() {
-        return cardList.stream().mapToInt(card -> {
-            int rank = card.getRank();
-            if (rank > 10) {
-                return 10;
-            }
-            return rank;
-        }).sum();
+        int sum = 0;
+        int aceCount = 0;
+        //ace는 보류
+        for(int i = 0; i<cardList.size(); i++){
+            int rank = cardList.get(i).getRank();
+            if(rank > 10) sum+=10;
+            else if(rank == 1) aceCount++;
+            else sum+=rank;
+        }
+        //ace가 두개 이상이라도 어차피 ace는 두개 이상 추가해줄 수 없으므로 하나 11점, 나머지 1점으로 가정해줘도 되는지 확인한 후 안되면 모두 1점
+        if(aceCount>0) {
+            if (sum + (aceCount - 1) + 11 <= 21)
+                sum += (aceCount - 1) + 11;
+            else
+                sum += aceCount;
+        }
+        return sum;
+
     }
 
     public void reset() {
